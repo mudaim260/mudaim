557a262727079bd2f74b57351189a9f8a08e89d9
mudaim260 <153026187+mudaim260@users.noreply.github.com>
2023-12-06 11:28:56 +0700

    Add files via upload

---
+++ pengalaman.py
@@ -0,0 +1,34 @@
+
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Organizational Experience|Syarifah",
+    page_icon="💃",
+    layout="wide"
+)
+
+st.title("Pengalaman Organisasi✨")
+st.write(" Organisasi Yang Saya Ikuti; ")
+
+st.container()
+col1, = st.columns(1)
+with col1:
+    st.subheader(">PANITIA ALFIYAH IBNU MALIK selama 3 Tahun 2020 - 2023")
+    st.subheader(">ISIM selama 2 Tahun 2017 - 2018")
+    st.subheader(">Panitia PPDB selama 4 tahun 2019 - 2023")
+
+
+st.image("panitia.jpg", width=200)
+st.image("isim.jpg", width=200)
+st.image("ppdb.jpg", width=200)
+st.container()
+col1, = st.columns(1)
+with col1:
+    st.subheader(">Menjadi Panitia Dekdok Dekorasi Dokumentasi")
+    st.subheader(">Menjadi seksi seni kebudayaan")
+    st.subheader(">Menjadi seksi seni Pemberkasan")
+
+
+
+
